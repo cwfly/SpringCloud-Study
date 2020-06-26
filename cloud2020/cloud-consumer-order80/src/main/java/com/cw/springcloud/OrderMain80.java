7df2aef22e6c73791dea07400b5837dfd771871a
cwfly <1129367293@qq.com>
2020-06-26 21:58:28 +0800

    服务注册

--- cloud2020/cloud-consumer-order80/src/main/java/com/cw/springcloud/OrderMain80.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/cw/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.cw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author cwfly
  * @date 2020/6/26
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud2020/cloud-eureka-server7001/src/main/java/com/cw/springcloud/EurekaMain7001.java
+++ cloud2020/cloud-eureka-server7001/src/main/java/com/cw/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.cw.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author cwfly
  * @date 2020/6/26
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
